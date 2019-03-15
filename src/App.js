21147a32f00c5caa4fb7e1187c850c0932da3c21
Corey Ladovsky <coreyskymusic@gmail.com>
2019-03-15 13:10:13 -0400

    getting all commits per student to display

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { USER } from './config/config';
+import DateDisplay from './DateDisplay';
 // import './App.css';
 
 class App extends Component {
   state =
   {
+    allStudents: {
     "Isa Alvarado": "isaink/Web_Final_Practical",
     "Jonathan Andrade": "xpectro93/webfinal",
     "Erick Arellano": "earellano1719/technicalFinal",
@@ -13,45 +15,104 @@ class App extends Component {
     "Jonathan Bayne": "baynejd/Web_Final_Practical",
     "Treagan Birbal": "treaganbirbal/Web_Final_Exam",
     "Andres Cabrera": "acabrera100/Web_Final_Practical",
-    "Jonathan Erquinigo": "https://github.com/jerquinigo/unit6FinalExamEarWormReport",
-    "Evgeniya Ezhova": "https://github.com/evgeniyaezhova/EarwormReport",
-    "Diana Gaona": "https://github.com/dianacgaona/web_final_practical",
-    "Jung Rae Jang": "https://github.com/jungraejang/technical_assessment_practical",
-    "Morteza Khaki": "https://github.com/khakimorteza/Comprehensive-Technical-Assessment-Practical",
-    "Andrew Kil": "https://github.com/Andrew-Kil/Web_Final_Practical",
-    "Kelly Liang": "https://github.com/kellyliang7/Web_Final_Practical",
-    "Nicolle Loyaza": "https://github.com/NikkiVee/EarWorm",
-    "Jean Max Mezalon": "https://github.com/jmezalon/Max_Mezalon_Final_Practical",
-    "Mateo Navarrete": "https://github.com/mateo-navarrete/finally",
-    "Jacky Ong": "https://github.com/JJGITTY2018/Web_Final_Private",
-    "Deyvi Ortiz": "https://github.com/SurgamSurgam/web_final_practical",
-    "Tyson Pan": "https://github.com/ThaiSonP/finalAssesment",
-    "Alex Park": "https://github.com/ajinsoopark/Final-Exam",
-    "Thomas Perez": "https://github.com/Jevit01/WEB-FINAL-EXAM",
-    "Wynter Reid": "https://github.com/wynterreid/Finalllll",
-    "Muna Sharma": "https://github.com/munarsharma/comprehensive-assessment",
-    "T'Keya Stevens": "https://github.com/Pixelynx/unit6test_TKeyaStevens",
-    "Carina Taveras": "https://github.com/tCarina/earwormapp",
-    "Michell Tejada": "https://github.com/mitejada/Web_Final_Practical",
-    "Nielene Thomas": "https://github.com/Nielene/Unit_5_Final",
+    "Jonathan Erquinigo": "jerquinigo/unit6FinalExamEarWormReport",
+    "Evgeniya Ezhova": "evgeniyaezhova/EarwormReport",
+    "Diana Gaona": "dianacgaona/web_final_practical",
+    "Jung Rae Jang": "jungraejang/technical_assessment_practical",
+    "Morteza Khaki": "khakimorteza/Comprehensive-Technical-Assessment-Practical",
+    "Andrew Kil": "Andrew-Kil/Web_Final_Practical",
+    "Kelly Liang": "kellyliang7/Web_Final_Practical",
+    "Nicolle Loyaza": "NikkiVee/EarWorm",
+    "Jean Max Mezalon": "jmezalon/Max_Mezalon_Final_Practical",
+    "Mateo Navarrete": "mateo-navarrete/finally",
+    "Jacky Ong": "JJGITTY2018/Web_Final_Private",
+    "Deyvi Ortiz": "SurgamSurgam/web_final_practical",
+    "Tyson Pan": "ThaiSonP/finalAssesment",
+    "Alex Park": "ajinsoopark/Final-Exam",
+    "Thomas Perez": "Jevit01/WEB-FINAL-EXAM",
+    // "Wynter Reid": "wynterreid/Finalllll",
+    "Muna Sharma": "munarsharma/comprehensive-assessment",
+    "T'Keya Stevens": "Pixelynx/unit6test_TKeyaStevens",
+    "Carina Taveras": "tCarina/earwormapp",
+    "Michell Tejada": "mitejada/Web_Final_Practical",
+    "Nielene Thomas": "Nielene/Unit_5_Final",
+  },
+  studentLookup: {
+      isaink: 'Isa Alvarado',
+      xpectro93: 'Jonathan Andrade',
+      earellano1719: 'Erick Arellano',
+      jbain424: 'Jonelle Bain',
+      baynejd: 'Jonathan Bayne',
+      treaganbirbal: 'Treagan Birbal',
+      acabrera100: 'Andres Cabrera',
+      jerquinigo: 'Jonathan Erquinigo',
+      evgeniyaezhova: 'Evgeniya Ezhova',
+      dianacgaona: 'Diana Gaona',
+      jungraejang: 'Jung Rae Jang',
+      khakimorteza: 'Morteza Khaki',
+      'Andrew-Kil': 'Andrew Kil',
+      kellyliang7: 'Kelly Liang',
+      NikkiVee: 'Nicolle Loyaza',
+      jmezalon: 'Jean Max Mezalon',
+      'mateo-navarrete': 'Mateo Navarrete',
+      JJGITTY2018: 'Jacky Ong',
+      SurgamSurgam: 'Deyvi Ortiz',
+      ThaiSonP: 'Tyson Pan',
+      ajinsoopark: 'Alex Park',
+      Jevit01: 'Thomas Perez',
+      wynterreid: 'Wynter Reid',
+      munarsharma: 'Muna Sharma',
+      Pixelynx: 'T\'Keya Stevens',
+      tCarina: 'Carina Taveras',
+      mitejada: 'Michell Tejada',
+      Nielene: 'Nielene Thomas'
+    },
+    allCommits: []
 }
+
   componentDidMount() {
-    axios({
+    let commitList = Object.values(this.state.allStudents).map(student => this.makeRequest(student))
+    Promise.all(commitList)
+    .then(res => {
+      let individualCommits = res.map(obj => obj.data)
+      this.setState({allCommits: individualCommits})
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+
+  makeRequest = (userRepo) => {
+    return axios({
       method: "get",
-      url: "https://api.github.com/repos/mateo-navarrete/finally/commits",
+      url: "https://api.github.com/repos/" + userRepo + "/commits",
       auth: {
         username: USER.NAME,
         password: USER.PASSWORD
       }
     })
-    .then(res => {
-      debugger
-    })
   }
+
   render() {
+    let displayPerson = this.state.allCommits.map(person => {
+      // debugger
+      console.log(person);
+      return(
+        <div >
+          <br/>
+          <br/>
+          <br/>
+          Name: {this.state.studentLookup[person[0].comments_url.slice(29).split("/")[0]]}
+          <br/>
+          TOTAL PROJECT COMMITS: {person.length}
+          <br/>
+            <DateDisplay array={person} />
+        </div>
+      )
+    })
     return (
       <div className="App">
-
+        {displayPerson}
       </div>
     );
   }
---
+++ src/DateDisplay.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+class DateDisplay extends React.Component {
+  render () {
+    let dates = {
+
+    }
+    this.props.array.forEach(array => {
+      let date = array.commit.author.date.slice(5, 10)
+      dates[date] ? dates[date]++ : dates[date] = 1;
+    })
+    let display = Object.keys(dates).map(date => {
+
+      return(
+        <li>
+          {date}: {dates[date]}
+        </li>
+      )
+    })
+    return(
+      <div>
+        Todays Commits: {dates["0" + (new Date().getMonth() + 1 )+ "-" + new Date().getDate()]}
+        <ul>
+          {display}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default DateDisplay;
