4bce456b9986ffa28037c058d11f2e4eb95408e7
Corey Ladovsky <coreyskymusic@gmail.com>
2019-03-16 16:41:46 -0400

    all students working with multiple sorting options

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,67 +6,67 @@ import "./App.css";
 class App extends Component {
   state = {
     allStudents: {
-      // "Isa Alvarado": "isaink/Web_Final_Practical",
-      // "Jonathan Andrade": "xpectro93/webfinal",
-      // "Erick Arellano": "earellano1719/technicalFinal",
-      // "Jonelle Bain": "jbain424/WebFinal",
-      // "Jonathan Bayne": "baynejd/Web_Final_Practical",
-      // "Treagan Birbal": "treaganbirbal/Web_Final_Exam",
-      // "Andres Cabrera": "acabrera100/Web_Final_Practical",
-      // "Jonathan Erquinigo": "jerquinigo/unit6FinalExamEarWormReport",
-      // "Evgeniya Ezhova": "evgeniyaezhova/EarwormReport",
+      "Isa Alvarado": "isaink/Web_Final_Practical",
+      "Jonathan Andrade": "xpectro93/webfinal",
+      "Erick Arellano": "earellano1719/technicalFinal",
+      "Jonelle Bain": "jbain424/WebFinal",
+      "Jonathan Bayne": "baynejd/Web_Final_Practical",
+      "Treagan Birbal": "treaganbirbal/Web_Final_Exam",
+      "Andres Cabrera": "acabrera100/Web_Final_Practical",
+      "Jonathan Erquinigo": "jerquinigo/unit6FinalExamEarWormReport",
+      "Evgeniya Ezhova": "evgeniyaezhova/EarwormReport",
       "Diana Gaona": "dianacgaona/web_final_practical",
-      // "Jung Rae Jang": "jungraejang/technical_assessment_practical",
-      // "Morteza Khaki":
-      //   "khakimorteza/Comprehensive-Technical-Assessment-Practical",
+      "Jung Rae Jang": "jungraejang/technical_assessment_practical",
+      "Morteza Khaki":
+        "khakimorteza/Comprehensive-Technical-Assessment-Practical",
       "Andrew Kil": "Andrew-Kil/Web_Final_Practical",
-      // "Kelly Liang": "kellyliang7/Web_Final_Practical",
-      // "Nicolle Loyaza": "NikkiVee/EarWorm",
+      "Kelly Liang": "kellyliang7/Web_Final_Practical",
+      "Nicolle Loyaza": "NikkiVee/EarWorm",
       "Jean Max Mezalon": "jmezalon/Max_Mezalon_Final_Practical",
-      "Mateo Navarrete": "mateo-navarrete/finally"
-      // "Jacky Ong": "JJGITTY2018/Web_Final_Private",
-      // "Deyvi Ortiz": "SurgamSurgam/web_final_practical",
-      // "Tyson Pan": "ThaiSonP/finalAssesment",
-      // "Alex Park": "ajinsoopark/Final-Exam",
-      // "Thomas Perez": "Jevit01/WEB-FINAL-EXAM",
-      // "Wynter Reid": "wynterreid/Finalllll",
-      // "Muna Sharma": "munarsharma/comprehensive-assessment",
-      // "T'Keya Stevens": "Pixelynx/unit6test_TKeyaStevens",
-      // "Carina Taveras": "tCarina/earwormapp",
-      // "Michell Tejada": "mitejada/Web_Final_Practical",
-      // "Nielene Thomas": "Nielene/Unit_5_Final",
-      // "Leo Lu": "Godsby/Web_Final_Exam"
+      "Mateo Navarrete": "mateo-navarrete/finally",
+      "Jacky Ong": "JJGITTY2018/Web_Final_Private",
+      "Deyvi Ortiz": "SurgamSurgam/web_final_practical",
+      "Tyson Pan": "ThaiSonP/finalAssesment",
+      "Alex Park": "ajinsoopark/Final-Exam",
+      "Thomas Perez": "Jevit01/WEB-FINAL-EXAM",
+      "Wynter Reid": "wynterreid/Finalllll",
+      "Muna Sharma": "munarsharma/comprehensive-assessment",
+      "T'Keya Stevens": "Pixelynx/unit6test_TKeyaStevens",
+      "Carina Taveras": "tCarina/earwormapp",
+      "Michell Tejada": "mitejada/Web_Final_Practical",
+      "Nielene Thomas": "Nielene/Unit_5_Final",
+      "Leo Lu": "Godsby/Web_Final_Exam"
     },
     studentLookup: {
-      // isaink: "Isa Alvarado",
-      // xpectro93: "Jonathan Andrade",
-      // earellano1719: "Erick Arellano",
-      // jbain424: "Jonelle Bain",
-      // baynejd: "Jonathan Bayne",
-      // treaganbirbal: "Treagan Birbal",
-      // acabrera100: "Andres Cabrera",
-      // jerquinigo: "Jonathan Erquinigo",
-      // evgeniyaezhova: "Evgeniya Ezhova",
+      isaink: "Isa Alvarado",
+      xpectro93: "Jonathan Andrade",
+      earellano1719: "Erick Arellano",
+      jbain424: "Jonelle Bain",
+      baynejd: "Jonathan Bayne",
+      treaganbirbal: "Treagan Birbal",
+      acabrera100: "Andres Cabrera",
+      jerquinigo: "Jonathan Erquinigo",
+      evgeniyaezhova: "Evgeniya Ezhova",
       dianacgaona: "Diana Gaona",
-      // jungraejang: "Jung Rae Jang",
-      // khakimorteza: "Morteza Khaki",
+      jungraejang: "Jung Rae Jang",
+      khakimorteza: "Morteza Khaki",
       "Andrew-Kil": "Andrew Kil",
-      // kellyliang7: "Kelly Liang",
-      // NikkiVee: "Nicolle Loyaza",
+      kellyliang7: "Kelly Liang",
+      NikkiVee: "Nicolle Loyaza",
       jmezalon: "Jean Max Mezalon",
-      "mateo-navarrete": "Mateo Navarrete"
-      // JJGITTY2018: "Jacky Ong",
-      // SurgamSurgam: "Deyvi Ortiz",
-      // ThaiSonP: "Tyson Pan",
-      // ajinsoopark: "Alex Park",
-      // Jevit01: "Thomas Perez",
-      // wynterreid: "Wynter Reid",
-      // munarsharma: "Muna Sharma",
-      // Pixelynx: "T'Keya Stevens",
-      // tCarina: "Carina Taveras",
-      // mitejada: "Michell Tejada",
-      // Nielene: "Nielene Thomas",
-      // Godsby: "Leo Lu"
+      "mateo-navarrete": "Mateo Navarrete",
+      "JJGITTY2018": "Jacky Ong",
+      SurgamSurgam: "Deyvi Ortiz",
+      ThaiSonP: "Tyson Pan",
+      ajinsoopark: "Alex Park",
+      Jevit01: "Thomas Perez",
+      wynterreid: "Wynter Reid",
+      munarsharma: "Muna Sharma",
+      Pixelynx: "T'Keya Stevens",
+      tCarina: "Carina Taveras",
+      mitejada: "Michell Tejada",
+      Nielene: "Nielene Thomas",
+      Godsby: "Leo Lu"
     },
     allCommits: [],
     search: "",
@@ -95,7 +95,7 @@ class App extends Component {
     this.setState({ [e.target.id]: e.target.value });
   };
 
-  render() {
+  countDailyCommits = person => {
     let months = [
       "Jan",
       "Feb",
@@ -110,6 +110,25 @@ class App extends Component {
       "Nov",
       "Dec"
     ];
+    let dates = {
+      [months[new Date().getMonth()] + " " + new Date().getDate()]: 0
+    };
+    person.forEach(array => {
+      let [month, day] = array.commit.author.date
+        .slice(5, 10)
+        .split("-")
+        .map(el => Number(el));
+      let date = months[month - 1] + " " + day;
+      dates[date] ? dates[date]++ : (dates[date] = 1);
+    });
+
+    let today =
+      months[new Date().getMonth()] + " " + new Date().getDate();
+    return dates[today];
+  }
+
+  render() {
+
     let displayPerson = this.state.allCommits
       .sort((a, b) => {
         debugger;
@@ -118,37 +137,12 @@ class App extends Component {
         } else if (this.state.sort === "total-asc") {
           return a.length - b.length;
         } else {
-          let aDates = {
-            [months[new Date().getMonth()] + " " + new Date().getDate()]: 0
-          };
-          let bDates = {
-            [months[new Date().getMonth()] + " " + new Date().getDate()]: 0
-          };
-
-          a.forEach(array => {
-            let [month, day] = array.commit.author.date
-              .slice(5, 10)
-              .split("-")
-              .map(el => Number(el));
-            let date = months[month - 1] + " " + day;
-            aDates[date] ? aDates[date]++ : (aDates[date] = 1);
-          });
-
-          b.forEach(array => {
-            let [month, day] = array.commit.author.date
-              .slice(5, 10)
-              .split("-")
-              .map(el => Number(el));
-            let date = months[month - 1] + " " + day;
-            bDates[date] ? bDates[date]++ : (bDates[date] = 1);
-          });
-
-          let today =
-            months[new Date().getMonth()] + " " + new Date().getDate();
+          let aToday = this.countDailyCommits(a)
+          let bToday = this.countDailyCommits(b)
           if (this.state.sort === "today-dec") {
-            return bDates[today] - aDates[today];
+            return bToday - aToday;
           } else if (this.state.sort === "today-asc") {
-            return aDates[today] - bDates[today];
+            return aToday - bToday;
           }
         }
       })
@@ -196,7 +190,7 @@ class App extends Component {
           <option value="total-dec">By Most Total Commits</option>
           <option value="total-asc">By Least Total Commits</option>
           <option value="today-dec">By Most Commits Today</option>
-          <option value="today-asc">By Least Commits Todayå</option>
+          <option value="today-asc">By Least Commits Today</option>
         </select>
 
         {displayPerson}
